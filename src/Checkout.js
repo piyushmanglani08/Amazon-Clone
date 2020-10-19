44113af4da01e3a8b5ad456f277253ee3f7e4e1d
piyushmanglani08 <piyushmanglani08@gmail.com>
2020-10-19 12:42:29 +0530

    home and checkout

---
+++ src/Checkout.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Route } from 'react-router-dom';
+import "./Checkout.css";
+function Checkout() {
+    return (
+        <div className="checkout">
+            <div className="checkout__right">
+                <img className="checkout__ad" src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Wireless/OnePlus/Jupiter/V252166690_Rush-Wireless-OnePlus_8T_Mobile_Launches_xbox._CB419091888_SY500_.jpg" alt="" />
+            <div>
+                <h2 className="checkout__title">
+                    Your shopping Basket
+                </h2>
+            </div>
+        </div>
+        <div className="checkout__right">
+        
+        </div>
+    </div>
+    )
+}
+
+export default Checkout;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDg0S-QIG-GwMxlyoZA8IAy0APLoiIV6Y4",
+    authDomain: "clone-1ec18.firebaseapp.com",
+    databaseURL: "https://clone-1ec18.firebaseio.com",
+    projectId: "clone-1ec18",
+    storageBucket: "clone-1ec18.appspot.com",
+    messagingSenderId: "838055770638",
+    appId: "1:838055770638:web:bd30488a82608d95fc9a5f",
+    measurementId: "G-GWD9N5QXZM"
+  };
