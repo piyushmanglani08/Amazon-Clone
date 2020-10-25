163b741265c85fbcfd902c5da341f59d325551e9
piyushmanglani08 <piyushmanglani08@gmail.com>
2020-10-25 22:21:29 +0530

    Login added header updated

--- src/App.js
+++ src/App.js
@@ -5,21 +5,24 @@ import Home from './Home';
 import {BrowserRouter as Router , Switch, Route}
 from "react-router-dom";
 import Checkout from './Checkout';
+import Login from './Login';
 
 function App() {
   return (
     <Router>
     <div className="app">
-    <Header />
-     <Switch>
     
+     <Switch>
+       <Route path="/login">
+          <Login />
+       </Route>
        <Route path="/checkout">
-          
+          <Header />
           <Checkout />
        </Route>
 
        <Route path="/">
-          
+       <Header />
           <Home />
        </Route>
      </Switch>
