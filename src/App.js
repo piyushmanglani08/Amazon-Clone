b2dd5598bf2e313fea35e7b8eb367b83372e15b9
piyushmanglani08 <piyushmanglani08@gmail.com>
2020-10-27 02:33:18 +0530

    Auth done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
@@ -6,8 +6,36 @@ import {BrowserRouter as Router , Switch, Route}
 from "react-router-dom";
 import Checkout from './Checkout';
 import Login from './Login';
+import {auth} from "./firebase";
+import { useStateValue } from './StateProvider';
+
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+
+  useEffect(() => {
+  // will only runs once when the app components loads
+  auth.onAuthStateChanged(authUser => {
+    console.log('The user is >>>',authUser);
+    if(authUser) {
+      //the user just logged in
+      dispatch({
+        type:'SET_USER',
+        user: authUser
+      }) 
+      }
+      else{
+        //the user is logged out
+        dispatch({
+
+        type:'SET_USER',
+        user: null
+        })
+      }
+    })     
+  },[])
+
   return (
     <Router>
     <div className="app">
--- src/Checkout.js
+++ src/Checkout.js
@@ -5,12 +5,13 @@ import CheckoutProduct from './CheckoutProduct';
 import { useStateValue } from './StateProvider';
 import Subtotal from './Subtotal';
 function Checkout() {
-    const [{basket},dispatch] = useStateValue();
+    const [{basket, user},dispatch] = useStateValue();
     return (
         <div className="checkout">
             <div className="checkout__right">
                 <img className="checkout__ad" src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Wireless/OnePlus/Jupiter/V252166690_Rush-Wireless-OnePlus_8T_Mobile_Launches_xbox._CB419091888_SY500_.jpg" alt="" />
             <div>
+                 
                 <h2 className="checkout__title">
                     Your shopping Basket
                 </h2>
