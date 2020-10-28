ce5740e465850ffe80e7e5fa641c8f2d7c1f8568
piyushmanglani08 <piyushmanglani08@gmail.com>
2020-10-28 21:19:26 +0530

    Minor changes

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,37 +1,43 @@
-import React from 'react'
-import { Route } from 'react-router-dom';
+import React from "react";
 import "./Checkout.css";
-import CheckoutProduct from './CheckoutProduct';
-import { useStateValue } from './StateProvider';
-import Subtotal from './Subtotal';
+import Subtotal from "./Subtotal";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
+
 function Checkout() {
-    const [{basket, user},dispatch] = useStateValue();
-    return (
-        <div className="checkout">
-            <div className="checkout__right">
-                <img className="checkout__ad" src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Wireless/OnePlus/Jupiter/V252166690_Rush-Wireless-OnePlus_8T_Mobile_Launches_xbox._CB419091888_SY500_.jpg" alt="" />
-            <div>
-                 
-                <h2 className="checkout__title">
-                    Your shopping Basket
-                </h2>
-                {basket.map(item => (
-                    <CheckoutProduct
-                    id = {item.id}
-                    title = {item.title}
-                    image = {item.image}
-                    price = {item.price}
-                    rating = {item.rating}
-                    />
-                ))}
-                
-            </div>
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt=""
+        />
+
+        <div>
+          <h3>Hello, {user?.email}</h3>
+          <h2 className="checkout__title">Your shopping Basket</h2>
+
+          {basket.map(item => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
+
         </div>
-        <div className="checkout__right">
+      </div>
+
+      <div className="checkout__right">
         <Subtotal />
-        </div>
+      </div>
     </div>
-    )
+  );
 }
 
 export default Checkout;
